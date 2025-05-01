95ba47cd0019021634117fe9f7fb6069523a8548
Owen <Owenpdonohue@gmail.com>
2025-05-01 15:13:15 -0700

    fixed endpoint for fetch for contact

--- webstormwebsite/src/components/Contact.jsx
+++ webstormwebsite/src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("https://owenportfolio.onrender.com/contact", {
+            const response = await fetch("https://charming-nature-production.up.railway.app/contact", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
